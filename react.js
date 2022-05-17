50951d0f0d4c7ceceecb8f0785679d15db9b8238
ashifayusaf <105366441+ashifayusaf@users.noreply.github.com>
2022-05-17 17:16:51 +0400

    Create react.js

---
+++ react.js
@@ -0,0 +1,31 @@
+import './App.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+
+const myele=
+(
+ <table>
+   <tr>
+   <th>name</th>
+   </tr>
+   <tr>
+     <td>asna</td>
+   </tr>
+   <tr>
+     <td>yusaf</td>
+   </tr>
+ </table>
+);
+ReactDOM.render(myele,document.getElementById('root'));
+function App() {
+  return (
+<div>
+  <h1>hello</h1>
+
+  <p>hasuh</p>
+</div>
+  );
+}
+
+export default App;
